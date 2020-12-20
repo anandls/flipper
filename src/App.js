522721afdecd437b1e7e8fffe22175b65492236e
Anand <anand@liquidsynergy.co.za>
2020-12-20 16:56:14 +0200

    updated App

--- src/App.js
+++ src/App.js
@@ -17,18 +17,18 @@ const App = () => {
   const [color, setColor] = React.useState(0);
   const colors = ['#FFC000', '#3386FF', '#247BA0', '#FF0000'];
 
- const handleChange = () => {
+  const handleChange = () => {
     color === colors.length - 1
-      ? setColor(0)
-      : setColor((prev) => prev + 1);
+    ? setColor(0)
+    : setColor((prev) => prev + 1);
   };
 
   return (
     <section>
-       <Button
-          handleChange={handleChange}
-          color={colors[color]}
-        />
+      <Button
+        handleChange={handleChange}
+        color={colors[color]}
+      />
     </section>
   );
 };
